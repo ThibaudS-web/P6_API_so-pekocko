851c90a059c4d4bf1ec892c904702d56baf2630f
ThibaudS <thibaud.devpro@gmail.com>
2021-06-03 10:26:24 +0200

    Added Router

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
-const mongoose = require('mongoose');
-const Sauce = require('./models/Sauce')
+const sauceRoutes = require('./routes/sauce')
+
 
 mongoose.connect('mongodb+srv://ThibS:14y3X3DDA3FPSrFA@cluster0.ffla6.mongodb.net/OPC-project_6?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -18,42 +19,7 @@ app.use((req, res, next) => {
     next()
   })
 
-app.post('/api/sauces', (req, res, next) => {
-  const sauce = new Sauce({
-    ...req.body
-  })
-  sauce.save()
-  .then(res.status(201).json({ message : "registered object !" }))
-  .catch(error => res.status(400).json({ error }))
-})
-
-app.post('/api/sauces/:id/like', (req, res, next) => {
-
-})
-
-app.put('/api/sauces/:id', (req, res, next) => {
-  Sauce.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
-  .then(() => res.status(200).json({ message: 'Object modified !'}))
-  .catch(error => res.status(400).json({ error }));
-})
-
-app.delete('/api/sauces/:id', (req, res, next) => {
-  Sauce.deleteOne({_id: req.params.id})
-  .then(() => res.status(200).json({ message: 'Object deleted !'}))
-  .catch(error => res.status(400).json({ error }));
-})
-
-app.get('/api/sauces', (req, res, next) => {
-  Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-})
-
-app.get('/api/sauces/:id', (req, res, next) => {
-  Sauce.findOne({_id: req.params.id})
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-})
+app.use('/api/sauces', sauceRoutes)
 
 module.exports = app
 
