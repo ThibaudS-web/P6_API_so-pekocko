d33d6c48d327766744b29f6ad78a0e471887111c
ThibaudS <thibaud.devpro@gmail.com>
2021-06-09 12:49:09 +0200

    Fixed POST /api/sauces and Added bodyParser.urlencoded on app.js

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const sauceRoutes = require('./routes/sauce')
-
 
 mongoose.connect('mongodb+srv://ThibS:14y3X3DDA3FPSrFA@cluster0.ffla6.mongodb.net/OPC-project_6?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
+  
 const app = express()
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use((req, res, next) =>   {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
     next()
-  })
+})
 
 app.use('/api/sauces', sauceRoutes)
 
