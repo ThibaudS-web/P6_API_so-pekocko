539d2d8caa27afccb3bf79b33bca5af83595d5a6
ThibaudS <thibaud.devpro@gmail.com>
2021-06-10 12:25:47 +0200

    Added router for post /auth and Updated app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 const sauceRoutes = require('./routes/sauce')
+const userRoutes = require('./routes/user')
 
 mongoose.connect('mongodb+srv://ThibS:14y3X3DDA3FPSrFA@cluster0.ffla6.mongodb.net/OPC-project_6?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -12,17 +13,17 @@ mongoose.connect('mongodb+srv://ThibS:14y3X3DDA3FPSrFA@cluster0.ffla6.mongodb.ne
   
 const app = express()
 
+app.use((req, res, next) =>   {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use((req, res, next) =>   {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    next()
-})
-
 app.use('/api/sauces', sauceRoutes)
+app.use('/api/auth', userRoutes)
 
 module.exports = app
-
