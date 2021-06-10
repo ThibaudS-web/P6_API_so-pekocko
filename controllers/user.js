f0e0374d9d81e38fa233562f9d524add620b9cf6
ThibaudS <thibaud.devpro@gmail.com>
2021-06-10 12:53:59 +0200

    Added controller logic for the login method

--- controllers/user.js
+++ controllers/user.js
@@ -17,5 +17,21 @@ exports.signup = (req, res, next) => {
 }
 
 exports.login = (req, res, next) => {
-    
+    User.findOne({ email: req.body.email} )
+        .then(user => {
+            if(!user) {
+                return res.status(401).json({ message:'User not found !'})
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if(!valid) {
+                        return res.status(401).json({ message:'Wrong password !'})
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    })
+                })
+        })
+        .catch(error => res.status(500).json({ error }))
 }
