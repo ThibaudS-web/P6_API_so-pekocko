4595408740f087619b0dec08c254c7ea4796a62d
ThibaudS <thibaud.devpro@gmail.com>
2021-06-24 10:12:35 +0200

    Updated POST like - Added .save() for DB

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -28,7 +28,6 @@ exports.createSauce = (req, res, next) => {
   } else {
     return res.status(401).json({ message : "Form is invalid !" })
   }
-
 }
 
 exports.rateSauce = (req, res, next) => {
@@ -42,18 +41,24 @@ exports.rateSauce = (req, res, next) => {
           console.log(sauce.dislikes)
           sauce.usersDisliked.push(req.body.userId)
           console.log(sauce)
-          res.status(200).json({ message : "You dislike this sauce !"})
+          sauce.save()
+          .then(() => res.status(200).json({ message : "You dislike this sauce !"}))
+          .catch(error => res.status(400).json({ error }))
           break
         case 0:
           console.log("0")
           if (sauce.usersDisliked.find(user => user === req.body.userId)) {
             sauce.usersDisliked = sauce.usersDisliked.filter(user => user !== req.body.userId)
             sauce.dislikes = sauce.dislikes - 1
-            res.status(200).json({ message : "Dislike deleted !"})
+            sauce.save()
+            .then(() => res.status(200).json({ message : "Dislike deleted !"}))
+            .catch(error => res.status(400).json({ error }))
           } else {
             sauce.usersLiked = sauce.usersDisliked.filter(user => user !== req.body.userId)
             sauce.likes = sauce.likes -1
-            res.status(200).json({ message : "Like deleted !"})
+            sauce.save()
+            .then(() => res.status(200).json({ message : "Like deleted !"}))
+            .catch(error => res.status(400).json({ error }))
           }        
           break
         case 1:
@@ -61,7 +66,9 @@ exports.rateSauce = (req, res, next) => {
           sauce.likes = sauce.likes + 1
           sauce.usersLiked.push(sauce.userId)
           console.log(sauce)
-          res.status(200).json({ message : "You like this sauce !"})
+          sauce.save()
+          .then(() => res.status(200).json({ message : "You like this sauce !"}))
+          .catch(error => res.status(400).json({ error }))
           break
         default:
           res.status(500).json({ error })
